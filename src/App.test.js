fda5cbf2d49f3ac4bd3c8f4c5f78ee5779fa300c
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2018-09-23 13:47:23 -0600

    Final upload before ejecting

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  // beforeEach = () => {
+  //   let wrapper = shallow(<App />);
+  //   console.log(wrapper);
+  // };
+
+  it('Should load with input', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('input').toHaveLength(1));
+  });
 });
+
+
